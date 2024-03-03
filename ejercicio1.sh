6f10b43f48ee44db8eac0de6f8a4bd513bef5d0a
omar-cruz <93933805+omar-cruz@users.noreply.github.com>
2024-03-03 00:39:09 -0600

    Omar_Cruz_03.03.2024

--- ejercicio1.sh
+++ ejercicio1.sh
@@ -3,7 +3,8 @@
 archivo="Accidentes_ags_2021.csv"
 word="ADOLFO"
 # Verificar
- salida=$(awk '{print $0" "$18}')
+ salida=$(awk '{print $0" "$18}') #falto agregar este comando al grep junto con el column
+if (grep -i "$word" "$archivo") 
 
 if(grep -i "$word" "$archivo")
 then
--- ejercicio2.sh
+++ ejercicio2.sh
@@ -2,7 +2,7 @@
 archivo="Accidentes_ags_2021.csv"
 word="AGUASCALIENTES"
 # Verificar
-salida=$(awk '{print $0" "$23}') 
+salida=$(awk '{print $0" "$23}') #falto agregar este comando al grep junto con el column
 if (grep -i "$word" "$archivo") 
 then
 echo "Aguascalientes no existe" $archivo
